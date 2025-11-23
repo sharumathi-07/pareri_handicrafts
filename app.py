03da8f7478efe817b5bfd6eccc52af622a9ef1a7
sharumathi-07 <sharumathi797@gmail.com>
2025-11-23 14:30:16 +0530

    app.py

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
 import streamlit as st
 import os
-
-# Get the directory where app.py is located
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+def read_file(path):
+    with open(path, "r", encoding="utf-8") as f:
+        return f.read()
+page = st.sidebar.selectbox(
+    "Choose Page",
+    ["Home (index.html)", "Products (product.html)"]
+)
+if page == "Home (index.html)":
+    html_path = os.path.join(BASE_DIR, "index.html")
+    css_path = os.path.join(BASE_DIR, "style.css")
+else:
+    html_path = os.path.join(BASE_DIR, "product.html")
+    css_path = os.path.join(BASE_DIR, "style1.css")
+html_code = read_file(html_path)
+css_code = f"<style>{read_file(css_path)}</style>"
+final_html = css_code + html_code
+st.components.v1.html(final_html, height=6000, scrolling=True, width=None)
 
-# Path to index.html inside the same repo
-html_path = os.path.join(BASE_DIR, "index.html")
-
-# Read file
-with open(html_path, "r", encoding="utf-8") as f:
-    html_code = f.read()
-
-# Render HTML
-st.components.v1.html(html_code, height=800, scrolling=True)
